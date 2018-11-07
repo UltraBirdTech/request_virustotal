f21f1df7d9dc585e43f4d55cbaa11376b1cec255
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-08 07:13:59 +0900

    add shell_01.sh. about gunzip zip files.

---
+++ shell_01.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+clear
+ls -la /root/work/malware/downloads 
+
+cd /root/work/malware/downloads
+gunzip -v /root/work/malware/downloads/downloads.tgz.1.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.2.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.3.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.4.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.5.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.6.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.7.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.8.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.9.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.10.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.11.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.12.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.13.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.14.gz
+gunzip -v /root/work/malware/downloads/downloads.tgz.15.gz
