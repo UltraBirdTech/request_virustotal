5785bbdfd3566486ce8868369625fe4ccbf92ff6
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-11 12:32:20 +0900

    fix tar option.

--- shell_00.sh
+++ shell_00.sh
@@ -5,5 +5,5 @@ cp /data/cowire/downloads.*.gz /home/honey/downloads/
 echo 'copy folder is finish.'
 
 echo 'create tar file start.'
-tar -zxcf downloads.tar.gz /home/honey/downloads/
+tar -zcvf downloads.tar.gz /home/honey/downloads/
 echo 'create tar file finsih.'
