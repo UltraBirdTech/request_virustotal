7fbe89e13a417009d6c5005354ea6f7ba0224c7a
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-12 07:28:39 +0900

    check folder

--- shell_01.sh
+++ shell_01.sh
@@ -21,7 +21,10 @@ tar -xvf $MALWARE_DIR/downloads.tar -C $MALWARE_DIR
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 gunzip -v $MALWARE_DOWNLOAD_DIR/downloads.tgz.$i.gz
-# check exist folder
-mkdir $MALWARE_DOWNLOAD_DIR/downloads/data$i
+if [ ! -e $MALWARE_DOWNLOAD_DIR/data$i ]
+  then
+    echo 'create folder'
+    mkdir $MALWARE_DOWNLOAD_DIR/data$i
+  fi
 tar -xzvf $MALWARE_DOWNLOAD_DIR/downloads.tgz.$i -C $MALWARE_DOWNLOAD_DIR/data$i
 done
