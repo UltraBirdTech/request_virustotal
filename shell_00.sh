37694daf1d4b62e2f2e594897a738ad4c43b5332
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-11 15:48:31 +0900

    add create folder for shell 00

--- shell_00.sh
+++ shell_00.sh
@@ -1,9 +1,11 @@
 #!bin/sh
-
+echo 'create downloads folder is start.'
+mkdir ./downloads
+echo 'create downloads foler is finished.'
 echo 'copy folder is start.'
-cp /data/cowire/downloads.*.gz /home/honey/downloads/
+cp /data/cowrie/downloads.*.gz /home/honey/downloads/
 echo 'copy folder is finish.'
 
 echo 'create tar file start.'
-tar -zcvf downloads.tar.gz /home/honey/downloads/
+tar -zcvf downloads.tar.gz ./downloads/
 echo 'create tar file finsih.'
