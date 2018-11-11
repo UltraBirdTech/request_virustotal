fff2979a5404161e939a035d47995f11076b97c5
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-11 15:44:27 +0900

    add comment.

--- shell_01.sh
+++ shell_01.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+# check file
 mv /root/work/honeypot/downloads.tar.gz /root/work/malware/
 gunzip -v /root/work/malware/downloads.tar.gz
 tar -xvf /root/work/malware/downloads.tar -C /root/work/malware/
@@ -7,6 +8,7 @@ tar -xvf /root/work/malware/downloads.tar -C /root/work/malware/
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 gunzip -v /root/work/malware/downloads/downloads.tgz.$i.gz
+# check exist folder
 mkdir /root/work/malware/downloads/data$i
 tar -xzvf /root/work/malware/downloads/downloads.tgz.$i -C /root/work/malware/downloads/data$i
 done
