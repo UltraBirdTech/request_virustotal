9fffc3ef94ecd256015dcbc15e0fd3d669977ad9
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-12 07:34:41 +0900

    add comment and shape up.

--- shell_01.sh
+++ shell_01.sh
@@ -7,17 +7,18 @@ echo $HONEYPOT_DIR
 echo $MALWARE_DIR
 
 if [ ! -e $HONEYPOT_DIR/downloads.tar.gz ]
-	then
-		echo "Not Found File"
-		exit 0
-	else
-		echo "Found File"
-	fi
+  then
+    echo "Not Found File"
+    exit 0
+  else
+    echo "Found File"
+  fi
 
 mv $HONEYPOT_DIR/downloads.tar.gz $MALWARE_DIR
 gunzip -v $MALWARE_DIR/downloads.tar.gz
 tar -xvf $MALWARE_DIR/downloads.tar -C $MALWARE_DIR
 
+# sepalate from shell_01.sh. Maybe.. I should Shell_02.sh below scripts.
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15
 do
 gunzip -v $MALWARE_DOWNLOAD_DIR/downloads.tgz.$i.gz
