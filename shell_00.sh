2a917cfb404fe3e79f651357fc6c588bf779a744
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-11 12:31:18 +0900

    fix spell miss

--- shell_00.sh
+++ shell_00.sh
@@ -1,9 +1,9 @@
 #!bin/sh
 
 echo 'copy folder is start.'
-cp /data/cowire/donwloads.*.gz /home/honey/downloads/
+cp /data/cowire/downloads.*.gz /home/honey/downloads/
 echo 'copy folder is finish.'
 
 echo 'create tar file start.'
-tar -zxcf downloads.tar.gz /home/honey/donwloads/
+tar -zxcf downloads.tar.gz /home/honey/downloads/
 echo 'create tar file finsih.'
