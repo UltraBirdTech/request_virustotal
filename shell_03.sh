838254412a5e51136efebcfdb2634ed3bfdaf769
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-15 07:04:57 +0900

    add shell 03.sh

---
+++ shell_03.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+MALWARE_DOWNLOAD_DIR='/root/work/malware/downloads'
+
+num=$1
+if [ ! -n "$num" ]
+  then
+    num='7'
+elif [ $num -gt 15 ]
+  then
+    echo 'Option: Please input less 15.'
+    exit 0
+fi
+
+for i in `seq $num`
+do
+gunzip -v $MALWARE_DOWNLOAD_DIR/downloads.tgz.$i.gz
+if [ ! -e $MALWARE_DOWNLOAD_DIR/data$i ]
+  then
+    echo 'create folder'
+    mkdir $MALWARE_DOWNLOAD_DIR/data$i
+fi
+tar -xzvf $MALWARE_DOWNLOAD_DIR/downloads.tgz.$i -C $MALWARE_DOWNLOAD_DIR/data$i
+done
