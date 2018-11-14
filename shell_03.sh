c61859b0dab408c4d3583fd98ab2747cd2b7eb94
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-15 07:25:27 +0900

    add debugs

--- shell_03.sh
+++ shell_03.sh
@@ -2,9 +2,6 @@
 
 MALWARE_DOWNLOAD_DIR='/root/work/malware/downloads'
 
-
-echo ls $MALWARE_DOWNLOAD_DIR
-
 file_num=$(ls $MALWARE_DOWNLOAD_DIR | grep data* | wc -l)
 
 echo $file_num
@@ -16,8 +13,5 @@ if [ ! -e "$MALWARE_DOWNLOAD_DIR/malware" ]
 
 for i in `seq $file_num`
 do
-	echo $MALWARE_DOWNLOAD_DIR
-	echo $MALWARE_DOWNLOAD_DIR/data$i/data/cowrie/downloads
-	echo $i
-	cp $MALWARE_DOWNLOAD_DIR/data$i/data/cowrie/downloads/* $MALWARE_DOWNLOAD_DIR/malware
+  cp $MALWARE_DOWNLOAD_DIR/data$i/data/cowrie/downloads/* $MALWARE_DOWNLOAD_DIR/malware
 done
