47a7409977b498c309d3b2baaa38c71cf6e53060
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-15 07:28:19 +0900

    remove debug

--- shell_03.sh
+++ shell_03.sh
@@ -4,8 +4,6 @@ MALWARE_DOWNLOAD_DIR='/root/work/malware/downloads'
 
 file_num=$(ls $MALWARE_DOWNLOAD_DIR | grep data* | wc -l)
 
-echo $file_num
-
 if [ ! -e "$MALWARE_DOWNLOAD_DIR/malware" ]
   then
     mkdir $MALWARE_DOWNLOAD_DIR/malware
