cbf98bde9606b4d8c00f5589ad7534deac9909f4
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-18 12:54:04 +0900

    rm operate folders

--- shell_04.sh
+++ shell_04.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
 MALWARE_DOWNLOAD_DIR='/root/work/malware/downloads'
+MALWARE_DOWNLOAD_TAR='/root/work/malware/downloads.tar'
 
 rm -rf $MALWARE_DOWNLOAD_DIR/data*
 rm $MALWARE_DOWNLOAD_DIR/downloads.tgz.*
+rm -rf $MALWARE_DOWNLOAD_DIR
+rm $MALWARE_DOWNLOAD_TAR
