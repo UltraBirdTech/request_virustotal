fa9cfb3284ae12f23cff1e0a5c3b12b97235e4ff
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-18 14:00:42 +0900

    add comment and log message.

--- 02_thawing_tar.sh
+++ 02_thawing_tar.sh
@@ -5,12 +5,21 @@ MALWARE_DIR='/root/work/malware'
 DOWNLOAD_FILE='downloads.tar.gz'
 TAR_FILE='downloads.tar'
 
+echo 'start move a tar file from /root/work/honeypot to /root/work/malware'
 if [ ! -e $HONEYPOT_DIR/downloads.tar.gz ]
   then
     echo "Not Found File. Please check $HONEYPOT_DIR/$DOWNLOAD_FILE"
 else
     mv $HONEYPOT_DIR/$DOWNLOAD_FILE $MALWARE_DIR
   fi
+echo 'move a tar file is finishe.'
 
+######## gunzip tar file #############
+echo 'gunzip tar file'
 gunzip -v $MALWARE_DIR/$DOWNLOAD_FILE
+echo 'gunzip is finish'
+
+######## thawing tar file #############
+echo 'tar thawing is start'
 tar -xvf $MALWARE_DIR/$TAR_FILE -C $MALWARE_DIR
+echo 'tar thawing is finished.'
