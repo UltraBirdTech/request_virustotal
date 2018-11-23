2acfbd83b6b4363de44b5aad443680ae007f1d5c
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-23 18:41:07 +0900

    remove debug

--- check.py
+++ check.py
@@ -40,7 +40,6 @@ def main():
        file_timedate = datetime.fromtimestamp(time_float).strftime("%Y/%m/%d %H:%M:%S")
        result_array.append("|" + file_name + "|" + file_timedate + "|" + file_detection_rate + "|" + permalink + "|")
        if 4 == i:
-           break
            print '[LOG] Sleep 65 seconds.'
            sleep(65)
            i = 0
