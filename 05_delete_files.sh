ef07c82244fa2340b76855464598705ce899e6de
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-24 09:42:24 +0900

    change path

--- 05_delete_files.sh
+++ 05_delete_files.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-MALWARE_DOWNLOAD_DIR='/root/work/malware/downloads'
+MALWARE_DOWNLOAD_DIR='/root/work/malware/home'
 MALWARE_DOWNLOAD_TAR='/root/work/malware/downloads.tar'
 
 echo '[START]delete file and direcotry'
