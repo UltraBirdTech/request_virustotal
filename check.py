eac88e0d2cad0713be125c83098d1775cc6075c5
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-11-25 08:33:00 +0900

    trace case int to str

--- check.py
+++ check.py
@@ -54,7 +54,7 @@ class MalwareFile:
 class OutputFile:
     def generate(self, array):
         with open(self.generate_file_name(), 'w') as f:
-            f.writelines('Total: ' + len(array) + "\n")
+            f.writelines('Total: ' + str(len(array)) + "\n")
             f.writelines(self.header() + "\n")
             f.writelines(self.constitution() + "\n")
             for line in array:
