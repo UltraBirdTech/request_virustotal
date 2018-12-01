c2a3f17e4a3e7527fbe0aafcf43781accc6538b0
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-02 07:46:23 +0900

    change folder path.

--- 02_thawing_tar.sh
+++ 02_thawing_tar.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
 
-HONEYPOT_DIR='/root/work/honeypot'
+HONEYPOT_DIR='./downloads'
+DOWNLOAD_DIR='./downloads'
 MALWARE_DIR='/root/work/malware'
 DOWNLOAD_FILE='downloads.tar.gz'
 TAR_FILE='downloads.tar'
 
 echo 'start move a tar file from /root/work/honeypot to /root/work/malware'
-if [ ! -e $HONEYPOT_DIR/downloads.tar.gz ]
+if [ ! -e $DOWNLOAD_DIR/downloads.tar.gz ]
   then
-    echo "Not Found File. Please check $HONEYPOT_DIR/$DOWNLOAD_FILE"
+    echo "Not Found File. Please check $DOWNLOAD_DIR/$DOWNLOAD_FILE"
 else
-    mv $HONEYPOT_DIR/$DOWNLOAD_FILE $MALWARE_DIR
+#    mv $HONEYPOT_DIR/$DOWNLOAD_FILE $MALWARE_DIR
+    echo 'test'
   fi
 echo 'move a tar file is finishe.'
 
 ######## gunzip tar file #############
 echo 'gunzip tar file'
-gunzip -v $MALWARE_DIR/$DOWNLOAD_FILE
+#gunzip -v $MALWARE_DIR/$DOWNLOAD_FILE
+gunzip -v $DOWNLOAD_DIR/$DOWNLOAD_FILE
 echo 'gunzip is finish'
 
 ######## thawing tar file #############
 echo 'tar thawing is start'
-tar -xvf $MALWARE_DIR/$TAR_FILE -C $MALWARE_DIR
+tar -xvf $DOWNLOAD_DIR/$TAR_FILE -C $DOWNLOAD_DIR
 echo 'tar thawing is finished.'
