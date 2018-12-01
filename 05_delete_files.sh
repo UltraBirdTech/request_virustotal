08959bcce49607c7531cb0605f019777695efbe9
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-01 18:18:19 +0900

    remove file path.

--- 05_delete_files.sh
+++ 05_delete_files.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-MALWARE_DOWNLOAD_DIR='/root/work/malware/home/honey'
+MALWARE_DOWNLOAD_DIR='/root/work/malware/home'
 MALWARE_DOWNLOAD_TAR='/root/work/malware/downloads.tar'
 
 echo '[START]delete file and direcotry'
-rm -rf $MALWARE_DOWNLOAD_DIR/data*
-rm $MALWARE_DOWNLOAD_DIR/downloads.tgz.*
 rm -rf $MALWARE_DOWNLOAD_DIR
 rm $MALWARE_DOWNLOAD_TAR
 echo '[FINISH]delete file and direcotry'
