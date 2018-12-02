1099bead9636ba59519ee1840660f31aa083f576
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-02 14:11:17 +0900

    change verb name

--- 04_aggregation_shels.sh
+++ 04_aggregation_shels.sh
@@ -4,10 +4,10 @@ MALWARE_DOWNLOAD_DIR='./downloads'
 HONEYPOT_DIR='home/honey/downloads'
 DOWNLOAD_DIR='data/cowrie/downloads/*'
 
-file_num=$(ls $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR | grep data* | wc -l)
+folder_num=$(ls $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR | grep data* | wc -l)
 
 echo '[START]aggregnation shell.'
-echo "file num is $file_num"
+echo "folder num is $folder_num"
 if [ ! -e "$MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware" ]
   then
     mkdir $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware
@@ -15,7 +15,7 @@ else
     rm $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware/*
   fi
 
-for i in `seq $file_num`
+for i in `seq $folder_num`
 do
   cp -p $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/data$i/$DOWNLOAD_DIR $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware
 done
