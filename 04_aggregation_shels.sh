70e46fc4ce41fe0a6065145af974402c0d3260c7
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-02 14:22:20 +0900

    change folder path

--- 04_aggregation_shels.sh
+++ 04_aggregation_shels.sh
@@ -8,16 +8,16 @@ folder_num=$(ls $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR | grep data* | wc -l)
 
 echo '[START]aggregnation shell.'
 echo "folder num is $folder_num"
-if [ ! -e "$MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware" ]
+if [ ! -e "$MALWARE_DOWNLOAD_DIR/malware" ]
   then
-    mkdir $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware
+    mkdir $MALWARE_DOWNLOAD_DIR/malware
 else
-    rm $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware/*
+    rm $MALWARE_DOWNLOAD_DIR/malware/*
   fi
 
 for i in `seq $folder_num`
 do
-  cp -p $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/data$i/$DOWNLOAD_DIR $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/malware
+  cp -pu $MALWARE_DOWNLOAD_DIR/$HONEYPOT_DIR/data$i/$DOWNLOAD_DIR $MALWARE_DOWNLOAD_DIR/malware
 done
 
 echo '[FINISH]aggregnation shell.'
