b9e19d76005f26465a752dd018dbc53c4473a7cd
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-02 17:23:52 +0900

    add rm command.

--- 05_delete_files.sh
+++ 05_delete_files.sh
@@ -5,4 +5,5 @@ MALWARE_DOWNLOAD_DIR='./downloads'
 echo '[START]delete file and direcotry'
 rm $MALWARE_DOWNLOAD_DIR/downloads.tar
 rm -rf $MALWARE_DOWNLOAD_DIR/home
+rm -rf $MALWARE_DOWNLOAD_DIR/malware
 echo '[FINISH]delete file and direcotry'
