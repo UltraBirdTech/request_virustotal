6e7060bb8481129987ef9672ff369b410c95166d
UltraBirdTech <ultrabirdrepti@gmail.com>
2018-12-22 09:09:33 +0900

    add space.

--- check.py
+++ check.py
@@ -91,7 +91,7 @@ class MalwareFile:
     # 自分自身のファイルタイプを調査し格納する。
     # 内部的に Linux の file コマンドを打ち結果を格納する。
     def set_file_type(self):
-        proc = subprocess.Popen('file ' + MALWARE_DIR + self.file_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+        proc = subprocess.Popen( 'file ' + MALWARE_DIR + self.file_name, shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT )
         file_type = proc.stdout.readline()
         proc.poll()
         file_type_split = file_type.split(':')[-1]
