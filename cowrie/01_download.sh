c9fc7fdd84d6d98a292000b8371639c6bf35f8dc
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-01-14 08:32:43 +0900

    add download shell

--- cowrie/01_download.sh
+++ cowrie/01_download.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 HONEYPOT_DIR='./downloads'
-PEM='/Users/hatoritakuya/.ssh/honeypot.pem'
+PEM='/root/work/.ssh/honeypot.pem'
 PORT='64295'
 HONEY_DOMAIN='honey@ec2-13-114-189-98.ap-northeast-1.compute.amazonaws.com'
 HONEY_FILE_TAR='/home/honey/downloads.tar.gz'
