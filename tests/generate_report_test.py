4abf6bd0047d486f3f805295fc87dd04006126b7
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-01-20 17:42:52 +0900

    add test case.

--- tests/generate_report_test.py
+++ tests/generate_report_test.py
@@ -1,7 +1,17 @@
-from unittest import TestCase
+#from unittest import TestCase
+import unittest
 from generate_report import Cowrie
 
-class TestCowrie(TestCase):
+class TestCowrie(unittest.TestCase):
+  def setUp(self):
+    self.cowrie = Cowrie()
+
   def test_first_char(self):
-    cowrie = Cowrie()
-    self.assertEqual(cowrie.first_char(), 'c')
+    self.assertEqual(self.cowrie.first_char(), 'c')
+
+  @unittest.skip("I should create mock and reseponse from Mock")
+  def test_file_name(self):
+    pass
+    # Mock 使用してdatetime.now()の挙動を同じにする。
+    self.assertEqual(self.cowrie.file_name(), 'something write.')
+
