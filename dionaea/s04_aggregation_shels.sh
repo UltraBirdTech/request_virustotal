245db11bee606c58d1469fa5712cbdc92c371f01
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-03-24 21:18:52 +0900

    add new files

---
+++ dionaea/s04_aggregation_shels.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+MALWARE_DOWNLOAD_DIR='./downloads'
+HONEYPOT_DIR='home/honey/downloads'
+DOWNLOAD_DIR='data/dionaea/binaries/*'
+
+folder_num=$(ls $MALWARE_DOWNLOAD_DIR | grep data* | wc -l)
+
+echo '[START]aggregnation shell.'
+echo "folder num is $folder_num"
+if [ ! -e "$MALWARE_DOWNLOAD_DIR/malware" ]
+  then
+    echo 'create new folder'
+    mkdir $MALWARE_DOWNLOAD_DIR/malware
+else
+    echo 'delete previous files.'
+#    rm $MALWARE_DOWNLOAD_DIR/malware/*
+  fi
+
+echo 'aggregation files.'
+for i in `seq $folder_num`
+do
+  cp -p $MALWARE_DOWNLOAD_DIR/data$i/$DOWNLOAD_DIR $MALWARE_DOWNLOAD_DIR/malware
+done
+
+echo '[FINISH]aggregnation shell.'
