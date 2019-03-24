84e28c0acd970728695dbe7418d3d14f3a1c7b1d
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-03-24 19:36:00 +0900

    commit

---
+++ honey/02_remove_tar_file.sh
@@ -0,0 +1,15 @@
+#!bin/sh
+echo '============================'
+echo '=====START REMOVE FILES====='
+echo '============================'
+
+echo 'remove folder start.'
+rm -rf /home/honey/downloads
+rm -rf /home/honey/dionaea_malwares
+echo 'remove tar file start.'
+rm /home/honey/downloads.tar.gz
+rm /home/honey/dionaea_malwares.tar.gz
+
+echo '============================'
+echo '======END REMOVE FILES======'
+echo '============================'
