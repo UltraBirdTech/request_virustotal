7a6e9a231b9dc5d2b0d520e1ae4500aa2891e11d
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-03-30 15:11:04 +0900

    change verbs

--- dionaea/s05_delete_files.sh
+++ dionaea/s05_delete_files.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-MALWARE_DOWNLOAD_DIR='./downloads'
+OPERATE_DIONAEA_PATH='/home/honey/virustotal_api/dionaea'
+MALWARE_DOWNLOAD_DIR='downloads'
+OPERATE_PATH=$OPERATE_DIONAEA_PATH/$MALWARE_DOWNLOAD_DIR
 
 echo '[START]delete file and direcotry'
-rm $MALWARE_DOWNLOAD_DIR/dionaea_malwares.tar
-rm -rf $MALWARE_DOWNLOAD_DIR/home
-rm -rf $MALWARE_DOWNLOAD_DIR/malware
-rm -rf $MALWARE_DOWNLOAD_DIR/data*
-rm -rf $MALWARE_DOWNLOAD_DIR/binaries*
+rm $OPERATE_PATH/dionaea_malwares.tar
+rm -rf $OPERATE_PATH/home
+rm -rf $OPERATE_PATH/malware
+rm -rf $OPERATE_PATH/data*
+rm -rf $OPERATE_PATH/binaries*
 echo '[FINISH]delete file and direcotry'
