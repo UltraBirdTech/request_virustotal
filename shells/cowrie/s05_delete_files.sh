859e444b25aedbe46beb12016a647ec0178128da
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-03-30 14:49:01 +0900

    add shellsw

---
+++ shells/cowrie/s05_delete_files.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COWRIE_OPERATE_PATH='/home/honey/virustotal_api/cowrie'
+MALWARE_DOWNLOAD_DIR='downloads'
+OPERATE_PATH=$COWRIE_OPERATE_PATH/$MALWARE_DOWNLOAD_DIR
+
+echo '[START]delete file and direcotry'
+rm $OPERATE_PATH/downloads.tar
+rm -rf $OPERATE_PATH/home
+rm -rf $OPERATE_PATH/malware
+rm -rf $OPERATE_PATH/data*
+rm -rf $OPERATE_PATH/downloads*
+echo '[FINISH]delete file and direcotry'
