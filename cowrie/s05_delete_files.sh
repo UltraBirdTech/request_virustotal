157b7714e900975b6ced19e08ff23ec27c9801d2
UltraBirdTech <ultrabirdrepti@gmail.com>
2019-03-30 14:45:24 +0900

    change verb name

--- cowrie/s05_delete_files.sh
+++ cowrie/s05_delete_files.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-MALWARE_DOWNLOAD_DIR='./downloads'
+COWRIE_OPERATE_PATH='/home/honey/virustotal_api/cowrie'
+MALWARE_DOWNLOAD_DIR='downloads'
+OPERATE_PATH=$COWRIE_OPERATE_PATH/$MALWARE_DOWNLOAD_DIR
 
 echo '[START]delete file and direcotry'
-rm $MALWARE_DOWNLOAD_DIR/dionaea_malwares.tar
-rm -rf $MALWARE_DOWNLOAD_DIR/home
-rm -rf $MALWARE_DOWNLOAD_DIR/malware
-rm -rf $MALWARE_DOWNLOAD_DIR/data*
-rm -rf $MALWARE_DOWNLOAD_DIR/binaries*
+rm $OPERATE_PATH/downloads.tar
+rm -rf $OPERATE_PATH/home
+rm -rf $OPERATE_PATH/malware
+rm -rf $OPERATE_PATH/data*
+rm -rf $OPERATE_PATH/downloads*
 echo '[FINISH]delete file and direcotry'
